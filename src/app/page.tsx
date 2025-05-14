2cc50f63fdeb23fc30d91aef4ee1a7a53f0054a9
Justin Caovan <justincaovan@gmail.com>
2025-05-14 12:35:21 -0700

    temporarily disable auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default async function Example() {
       console.log(data)
     return (
         <>
-            <LoginAndLibrary />
+            {/* <LoginAndLibrary /> */}
             <Header />
 
             <div className="py-10">
